5de227978fb9ad5c04a7180b2b9beaca25d388af
Zach7893 <49004748+Zach7893@users.noreply.github.com>
2019-12-18 07:48:29 -0500

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,10 @@
+string = open('','r').readline()
+floor_count = 0
+for i in range(0,len(string)):
+    if string[i] == '(':
+        floor_count += 1
+    if string[i] == ')':
+        floor_count -= 1
+    if floor_count == -1:
+        print("step count is {}".format(i+1))         
+print(floor_count)
